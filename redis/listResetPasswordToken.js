55ff818d65c73cfe9b72ff7fdf773d63eb270e61
Guilherme Lorenzon Tempesta <guilhermetempesta@gmail.com>
2021-04-09 20:20:45 -0300

    tentativa Heroku Redis

--- redis/listResetPasswordToken.js
+++
@@ -1,6 +0,0 @@
-const redis = require('redis');
-const handleList = require('./handleList');
-
-const list = redis.createClient({ prefix: 'list-reset-password-token:' });
-
-module.exports = handleList(list);

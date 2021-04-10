51e6d317845437796c904f78d9cc22e22383d684
Guilherme Lorenzon Tempesta <guilhermetempesta@gmail.com>
2021-04-10 08:16:56 -0300

    alterar configurações do PostgreSQL para o Heroku

--- src/config/database.js
+++ src/config/database.js
@@ -16,11 +16,7 @@ module.exports = {
   },
   "production": {
     "dialect": "postgres",
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "host": process.env.DB_HOST,
-    "port": process.env.DB_PORT,
-    "database": process.env.DB_DATABASE,
+    "url": process.env.DATABASE_URL,
     "define": {
       "timestamps": true,
       "underscored": true

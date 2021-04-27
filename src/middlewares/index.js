c10c469b18d5b0645c6dd890c2d16bbe11f976c5
Guilherme Lorenzon Tempesta <guilhermetempesta@gmail.com>
2021-04-27 12:32:29 -0300

    configurar cors options para exibir header Authorization

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,8 +1,11 @@
 const bodyParser = require('body-parser')
 const cors = require('cors')
-
+const corsOptions = {
+    exposedHeaders: 'Authorization',
+};
+  
 module.exports = app => {
     app.use(bodyParser.json())
-    app.use(cors())
+    app.use(cors(corsOptions))
     app.disable('x-powered-by')
 }
